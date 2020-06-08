2d3499653cdee256e7ff03ce54a86ab33668e7c0
LuizQuirino <luizfpq@gmail.com>
2020-06-07 22:28:35 -0300

    add inserts, add docs, add Modelo Conceitual
    Co-Authored-By: null <lucaspadilha133@gmail.com>

---
+++ sql/inserts/02_tbl_aluno.sql
@@ -0,0 +1,6 @@
+insert into "tbl_aluno" (id_aluno, situacao, id_usuario) values (5835, 'pendente', 5)
+insert into "tbl_aluno" (id_aluno, situacao, id_usuario) values (2484, 'pendente', 6)
+insert into "tbl_aluno" (id_aluno, situacao, id_usuario) values (5573, 'pendente', 7)
+insert into "tbl_aluno" (id_aluno, situacao, id_usuario) values (3348, 'pendente', 8)
+insert into "tbl_aluno" (id_aluno, situacao, id_usuario) values (5078, 'pendente', 9)
+insert into "tbl_aluno" (id_aluno, situacao, id_usuario) values (6519, 'pendente', 10)
