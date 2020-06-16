297de86833744ac85ed3597c739a92cec4fe3d16
LuizQuirino <luizfpq@gmail.com>
2020-06-16 17:26:33 -0300

    deploy login page

--- app/view/logon/logon.php
+++ app/view/logon/logon.php
@@ -1,44 +1,53 @@
+<!doctype html>
 <html lang="pt-br">
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link rel="icon" href="assets/img/logo-2.png">
-  	<link href="https://fonts.googleapis.com/css?family=Poppins:100,400,800&display=swap" rel="stylesheet">
+    <meta name="description" content="Sistema de gestão de horas complementares - Kairós - UFMS">
+    <meta name="author" content="Lucas Padilha, Luiz Quirino">
+    <title> &middot; Kairós &middot;</title>
 
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.5/examples/sign-in/">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,100;0,300;0,400;0,500;0,700;1,100;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet">
 
-    <title>DOE-SE | Identifique-se</title>
+    <!-- Bootstrap core CSS -->
+<!-- <link href="../assets/css/bootstrap.css" rel="stylesheet"> -->
+<link href="assets/custom-style/signin.css" rel="stylesheet">
 
-    <!-- Principal CSS do Bootstrap -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/custom-style/signin.css">
-
+    <!-- Favicons -->
+<link rel="apple-touch-icon" href="assets/img/favicons/apple-touch-icon.png" sizes="180x180">
+<link rel="icon" href="assets/img/favicons/favicon-32x32.png" sizes="32x32" type="image/png">
+<link rel="icon" href="assets/img/favicons/favicon-16x16.png" sizes="16x16" type="image/png">
+<link rel="manifest" href="assets/img/favicons/manifest.json">
+<link rel="mask-icon" href="assets/img/favicons/safari-pinned-tab.svg" color="#563d7c">
+<link rel="icon" href="assets/img/favicons/favicon.ico">
+<meta name="msapplication-config" content="assets/img/favicons/browserconfig.xml">
+<meta name="theme-color" content="#563d7c">
   </head>
+  <body class="text-center">
 
-  <body class="text-center container-fluid">
+<img src="../assets/img/icon-kairos.png" class="icon-logo">
+<p name="logo" class="title-logo">
+  &middot; Kair&oacute;s
+</p>
+<div name="login-img-right" class="div-login-img-right">
+  <img src="../assets/img/blog-post-amico.png" class="login-img-right">
+</div>
 
-  	<div class="row h-100">
-  		<div class="col-lg-6 form-wrapper">
-  			<form class="form-signin" action="index.php?controller=Logon&action=logon" method="post">
-	  			<div class="container heading-form">
-	  				<div class="row brand">
-	  					<div class="col-4 col-lg-5"><img class="mb-4 float-right" src="assets/img/logo-2.png" alt="" width="72" height="72"></div>
-	  					<div class="col"><h1 class="text-left">DOE-SE</h1></div>
-	  				</div>
-	  			</div>
-		      <label for="inputEmail" class="sr-only">Endereço de email</label>
-		      <input type="email" name="email" id="inputEmail" class="form-control" placeholder="Seu email" required autofocus>
-		      <label for="inputPassword" class="sr-only">Senha</label>
-		      <input type="password" name="password" id="inputPassword" class="form-control" placeholder="Senha" required>
-		      <div class="checkbox mb-3">
-		        <label>
-		          <input type="checkbox" value="remember-me"> Lembrar de mim
-		        </label>
-		      </div>
-		      <button class="btn btn-lg btn-primary btn-block" type="submit">Login</button>
-		      <p class="mt-5 mb-3 text-muted">DOE-SE &copy; 2019</p>
-		    </form>
-  		</div>
-  		<div class="col-lg-6 hero-img"></div>
-  	</div>
-  </body>
+  <form class="form-signin">
+  <img class="mb-4" src="/docs/4.5/assets/brand/bootstrap-solid.svg" alt="" width="72" height="72">
+  <h1 class="form-signin-title-text">Login</h1>
+  <h2 class="form-signin-msg-text">Entre com sua conta</h2>
+  <input type="email" id="email" name="email" class="form-control" placeholder="Email" required autofocus>
+  <input type="password" id="password" name="password" class="form-control" placeholder="Senha" required>
+  <div class="checkbox mb-3">
+    <!--<label>
+      <input type="checkbox" value="remember-me"> Remember me
+    </label> -->
+    <br />
+  </div>
+  <button class="btn btn-lg btn-primary btn-primary-login-text" type="submit">Acessar</button>
+  
+</form>
+</body>
 </html>
--- installers/setup_deb.sh
+++
@@ -1,5 +0,0 @@
-# use `sudo` on Ubuntu or run this as root on debian
-apt-get install -y build-essential
-# Using Debian, as root
-sudo curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-sudo apt install -y nodejs
