3957b35a00b6362196027840bd70c78f07641976
LuizQuirino <luizfpq@gmail.com>
2020-06-16 19:15:24 -0300

    style mods

--- app/view/default/footer.php
+++ app/view/default/footer.php
@@ -3,7 +3,7 @@
 		<p class="text-muted w-100">
 			<img src="assets\img\logo-mono.png" width="32" style="margin-bottom: 5px;opacity: 0.6"><br>
 			DOE-SE &copy; 2019<br>
-			<small>Desenvolvido por José Vitor Gualdi, Lucas Fidelis, Lucas Padilha e Luiz F. P. Quirino.</small>
+			<small>Desenvolvido por Lucas Padilha e Luiz Quirino.</small>
 		</p>
 	</div>
 </footer>
