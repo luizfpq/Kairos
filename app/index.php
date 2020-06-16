69f10c58e2d56a348d2f8f1917e605bf86e6ba4b
LuizQuirino <luizfpq@gmail.com>
2020-06-16 17:11:46 -0300

    testing initial routes

--- app/index.php
+++ app/index.php
@@ -1,2 +1,24 @@
 <?php
-phpinfo();
+
+session_start();
+
+//unset($_SESSION);
+
+define('BASE_PATH', __DIR__);
+
+try
+{
+  require('autoload.php');
+
+  $controller = isset($_REQUEST['controller']) ? $_REQUEST['controller'] : 'Logon';
+
+  $action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'logon';
+
+  eval('$controller = new '. $controller .'Controller();');
+
+  eval('$controller->'. $action . 'Action();');
+}
+catch(Exception $e)
+{
+  die($e->getMessage());
+}
